deac44e0d80ca7c195309215e5f4043759c3d7b4
dellancelod <danil.svetaylo@gmail.com>
2023-07-25 19:44:29 +0300

    Add joystick movement

---
+++ Assets/AnimatorController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void PlayIdle()
+    {
+        animator.Play("Idle");
+    }
+    public void PlayWalk()
+    {
+        animator.Play("Walk");
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] 
+    private FloatingJoystick joystick;
+
+    [SerializeField] 
+    private AnimatorController animatorController;
+
+    [SerializeField]
+    public float moveSpeed;
+    [SerializeField]
+    public float rotationSpeed;
+
+    private Rigidbody rigidBody;
+
+    private Vector3 moveVector;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = joystick.Horizontal * moveSpeed * Time.deltaTime;
+        moveVector.z = joystick.Vertical * moveSpeed * Time.deltaTime;
+
+        if(joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            Vector3 direction = Vector3.RotateTowards(transform.forward, moveVector, rotationSpeed * Time.deltaTime, 0.0f);
+            transform.rotation = Quaternion.LookRotation(direction);
+
+            animatorController.PlayWalk();
+        }
+
+        else if (joystick.Horizontal == 0 && joystick.Vertical == 0)
+        {
+            animatorController.PlayIdle();
+        }
+
+        rigidBody.MovePosition(rigidBody.position + moveVector);
+    }
+}
